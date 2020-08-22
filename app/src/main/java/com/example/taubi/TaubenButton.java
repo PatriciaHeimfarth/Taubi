4dbdc485eb526df0f78724005be30425ab8900b4
Patti85 <pbeier24@googlemail.com>
2020-08-22 18:56:21 +0200

    new class TaubenButton

---
+++ app/src/main/java/com/example/taubi/TaubenButton.java
@@ -0,0 +1,9 @@
+package com.example.taubi;
+
+import android.content.Context;
+
+public class TaubenButton extends androidx.appcompat.widget.AppCompatButton {
+    public TaubenButton(Context context) {
+        super(context);
+    }
+}
